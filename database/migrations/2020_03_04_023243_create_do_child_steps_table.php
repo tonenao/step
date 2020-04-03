f27efbcd767ff56ce49f0624705a33550caed80b
とねなおや <tonenao@gmail.com>
2020-04-03 21:52:58 +0900

    デプロイ

---
+++ database/migrations/2020_03_04_023243_create_do_child_steps_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateDoChildStepsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('do_child_steps', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->integer('child_step_id');
+            $table->integer('user_id');
+            $table->string('comment')->nullable();
+            $table->boolean('delete_flg')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('do_child_steps');
+    }
+}
