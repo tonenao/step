113b1e2b6da58a751f5b438b2ae295e6ea0eb55c
とねなおや <tonenao@gmail.com>
2020-04-08 17:02:07 +0900

    3

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -30,5 +30,8 @@ class AppServiceProvider extends ServiceProvider
         if (\App::environment('production')) {
             \URL::forceScheme('https');
         }
+        if (request()->isSecure()) {
+            \URL::forceScheme('https');
+        }
     }
 }
