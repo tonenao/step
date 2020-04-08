18b77d50e09ea32d0662dd6ec4cc9153ea1f47aa
とねなおや <tonenao@gmail.com>
2020-04-08 15:31:22 +0900

    1

--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,16 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::middleware(['cors'])->group( function() {
+    Route::get('/step/json', 'StepsController@index_json')->name('steps.index_json');
+
+    Route::get('/favorite/json', 'StepsController@favorite_json')->name('steps.favorite_json');
+
+    Route::get('/mystep/json','StepsController@mystep_json')->name('steps.mystep_json');
+
+    Route::get('/mychallenge/json','StepsController@mychallenge_json')->name('steps.mychallenge_json');
+
+    Route::get('/step/json/{id}', 'StepsController@show_json')->name('step.show_json');
+    
+ });
--- routes/web.php
+++ routes/web.php
@@ -55,18 +55,18 @@ Route::post('/step/{id}/edit', 'StepsController@update')->name('steps.update');
 
 //middleware
 
-Route::middleware(['cors'])->group( function() {
-    Route::get('/step/json', 'StepsController@index_json')->name('steps.index_json');
+// Route::middleware(['cors'])->group( function() {
+//     Route::get('/step/json', 'StepsController@index_json')->name('steps.index_json');
 
-    Route::get('/favorite/json', 'StepsController@favorite_json')->name('steps.favorite_json');
+//     Route::get('/favorite/json', 'StepsController@favorite_json')->name('steps.favorite_json');
 
-    Route::get('/mystep/json','StepsController@mystep_json')->name('steps.mystep_json');
+//     Route::get('/mystep/json','StepsController@mystep_json')->name('steps.mystep_json');
 
-    Route::get('/mychallenge/json','StepsController@mychallenge_json')->name('steps.mychallenge_json');
+//     Route::get('/mychallenge/json','StepsController@mychallenge_json')->name('steps.mychallenge_json');
 
-    Route::get('/step/json/{id}', 'StepsController@show_json')->name('step.show_json');
+//     Route::get('/step/json/{id}', 'StepsController@show_json')->name('step.show_json');
     
- });
+//  });
 
 
 
