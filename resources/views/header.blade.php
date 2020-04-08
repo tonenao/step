20e0e638011ba6c0c9bac1769b8a6833bec77b39
とねなおや <tonenao@gmail.com>
2020-04-08 21:52:59 +0900

    l

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -42,7 +42,7 @@
               @if(Auth::user()->pic==='no-profile-image.png')
                 <img src="{{asset('img/no-profile-image.png')}}">
               @else
-                <img src="{{ asseet('storage/img/'.Auth::user()->pic) }}">
+                <img src="{{ asset('storage/img/'.Auth::user()->pic) }}">
               @endif
             </div>
 
