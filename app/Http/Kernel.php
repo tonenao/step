a9377a2a133c8f37945fb60c795754e6ccc71857
とねなおや <tonenao@gmail.com>
2020-04-08 15:57:18 +0900

    ローカルでは正常動作

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -19,8 +19,6 @@ class Kernel extends HttpKernel
         \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
         \App\Http\Middleware\TrimStrings::class,
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
-        \App\Http\Middleware\Cors::class,
-
     ];
 
     /**
@@ -40,7 +38,6 @@ class Kernel extends HttpKernel
         ],
 
         'api' => [
-
             'throttle:60,1',
             'bindings',
         ],
@@ -63,7 +60,6 @@ class Kernel extends HttpKernel
         'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
-        'cors'          => \App\Http\Middleware\Cors::class,
     ];
 
     /**
