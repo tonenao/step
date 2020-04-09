3ca389657a516836bd2dc758fa07d2432bf28b92
とねなおや <tonenao@gmail.com>
2020-04-10 03:01:12 +0900

    l

--- resources/lang/ja/passwords.php
+++ resources/lang/ja/passwords.php
@@ -13,10 +13,16 @@ return [
     |
     */
 
-    'password' => 'Passwords must be at least eight characters and match the confirmation.',
-    'reset' => 'Your password has been reset!',
-    'sent' => 'We have e-mailed your password reset link!',
-    'token' => 'This password reset token is invalid.',
-    'user' => "We can't find a user with that e-mail address.",
+    'password' => 'パスワードは8文字以上で、確認に一致している必要があります。',
+    'reset' => 'パスワードはリセットされました！',
+    'sent' => 'パスワードリセットリンクをメールで送信しました！',
+    'token' => 'このパスワードリセットトークンは無効です。',
+    'user' => "そのメールアドレスのユーザーは見つかりません。",
+
+    // 'password' => 'Passwords must be at least eight characters and match the confirmation.',
+    // 'reset' => 'Your password has been reset!',
+    // 'sent' => 'We have e-mailed your password reset link!',
+    // 'token' => 'This password reset token is invalid.',
+    // 'user' => "We can't find a user with that e-mail address.",
 
 ];
