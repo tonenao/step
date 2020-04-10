9c677f818582d58aae20d433f30b5786033f5a5b
とねなおや <tonenao@gmail.com>
2020-04-10 16:00:20 +0900

    l

--- public/js/app.js
+++ public/js/app.js
@@ -2011,7 +2011,7 @@ var axios = __webpack_require__(/*! axios */ "./node_modules/axios/index.js");
       });
     },
     createData: function createData($child_step_id) {
-      axios.post("/step/do_child/", {
+      axios.post("/step/do_child", {
         child_step_id: $child_step_id
       }).then(function (response) {
         console.log(response);
@@ -2023,7 +2023,7 @@ var axios = __webpack_require__(/*! axios */ "./node_modules/axios/index.js");
       this.$emit("click-inc-count");
     },
     removeData: function removeData($child_step_id) {
-      axios["delete"]("/step/do_child/", {
+      axios["delete"]("/step/do_child", {
         data: {
           child_step_id: $child_step_id
         }
