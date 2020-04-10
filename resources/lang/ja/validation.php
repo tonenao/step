b66c8933734d2ff5cd32348ce6db127598a42487
とねなおや <tonenao@gmail.com>
2020-04-10 10:29:16 +0900

    l

--- resources/lang/ja/validation.php
+++ resources/lang/ja/validation.php
@@ -79,7 +79,8 @@ return [
     'max' => [
         'numeric' => 'The :attribute may not be greater than :max.',
         'file' => 'The :attribute may not be greater than :max kilobytes.',
-        'string' => 'The :attribute may not be greater than :max characters.',
+        'string' => ':attribute は:max文字以内にしてください。',
+        // 'string' => 'The :attribute may not be greater than :max characters.',
         'array' => 'The :attribute may not have more than :max items.',
     ],
     'mimes' => 'The :attribute must be a file of type: :values.',
