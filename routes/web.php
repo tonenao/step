1cd487fe799779c9afdd7830f35fb6f02ff258da
とねなおや <tonenao@gmail.com>
2020-04-10 13:32:56 +0900

    l

--- routes/web.php
+++ routes/web.php
@@ -34,23 +34,23 @@ Route::post('/contact/complete', 'ContactsController@complete')->name('complete'
 
 Route::get('/', 'StepsController@top')->name('steps.top');
 
-Route::get('/step/', 'StepsController@index')->name('steps.index');
+Route::get('/step', 'StepsController@index')->name('steps.index');
 
 Route::get('/step/json', 'StepsController@index_json')->name('steps.index_json');
 
 Route::get('/favorite/json', 'StepsController@favorite_json')->name('steps.favorite_json');
 
 
-Route::get('/mypage/', 'StepsController@mypage')->name('steps.mypage');
+Route::get('/mypage', 'StepsController@mypage')->name('steps.mypage');
 
 Route::get('/mystep/json','StepsController@mystep_json')->name('steps.mystep_json');
 
 Route::get('/mychallenge/json','StepsController@mychallenge_json')->name('steps.mychallenge_json');
 
 
-Route::get('/step/new/', 'StepsController@new')->name('steps.new');
+Route::get('/step/new', 'StepsController@new')->name('steps.new');
 
-Route::post('/step/', 'StepsController@create')->name('steps.create');
+Route::post('/step', 'StepsController@create')->name('steps.create');
 
 
 Route::get('/step/json/{id}', 'StepsController@show_json')->name('step.show_json');
@@ -81,15 +81,15 @@ Route::post('/do_step/giveup', 'DoStepController@giveup');
 
 Route::post('/step/child_step','ChildStepController@create');
 
-Route::post('/step/{id}/child_step/','ChildStepController@update');
+Route::post('/step/{id}/child_step','ChildStepController@update');
 
 
 
 //DoChildStepController
 
-Route::post('/step/do_child/', 'DoChildStepController@create');
+Route::post('/step/do_child', 'DoChildStepController@create');
 
-Route::delete('/step/do_child/', 'DoChildStepController@delete');
+Route::delete('/step/do_child', 'DoChildStepController@delete');
 
 
 
