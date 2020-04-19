8c0c90549dc33a82f64cec913d37bc0863a83d07
とねなおや <tonenao@gmail.com>
2020-04-19 12:12:02 +0900

    テスト用

--- app/Http/Controllers/Auth/ForgotPasswordController.php
+++ app/Http/Controllers/Auth/ForgotPasswordController.php
@@ -21,6 +21,27 @@ class ForgotPasswordController extends Controller
 
     use SendsPasswordResetEmails;
 
+
+    //実験用
+    public function sendResetLinkEmail(Request $request)
+    {
+        $this->validateEmail($request);
+
+        // We will send the password reset link to this user. Once we have attempted
+        // to send the link, we will examine the response then see the message we
+        // need to show to the user. Finally, we'll send out a proper response.
+        $response = $this->broker()->sendResetLink(
+            $this->credentials($request)
+        );
+
+        return $response == Password::RESET_LINK_SENT
+                    ? $this->sendResetLinkFailedResponse($request, $response)
+                    : $this->sendResetLinkResponse($request, $response);
+                    // ? $this->sendResetLinkResponse($request, $response)
+                    // : $this->sendResetLinkFailedResponse($request, $response);
+    }
+
+
     //パスワードリセット メール送信時処理
     protected function sendResetLinkResponse(Request $request, $response)
     {
